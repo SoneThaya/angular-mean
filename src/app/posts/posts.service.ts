dd17aa90ebf35c0e575d374924605241e57b60e3
Sone <sthaya@gmail.com>
2021-03-10 10:17:58 -0800

    created edit form

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -35,6 +35,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return { ...this.posts.find((p) => p.id === id) };
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
@@ -48,6 +52,10 @@ export class PostsService {
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content };
   }
 
   deletePost(postId: string) {
