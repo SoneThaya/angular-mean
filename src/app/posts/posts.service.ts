284addfb2da0ed66cb455cb7130a69d6b920acbf
Sone <sthaya@gmail.com>
2021-03-10 15:09:31 -0800

    worked on edit post, and refactored app file to router posts

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
 import { map } from 'rxjs/operators';
+import { PortalHostDirective } from '@angular/cdk/portal';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -36,7 +37,9 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return { ...this.posts.find((p) => p.id === id) };
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      'http://localhost:3000/api/posts/' + id
+    );
   }
 
   addPost(title: string, content: string) {
@@ -56,6 +59,15 @@ export class PostsService {
 
   updatePost(id: string, title: string, content: string) {
     const post: Post = { id: id, title: title, content: content };
+    this.http
+      .put('http://localhost:3000/api/posts' + id, post)
+      .subscribe((response) => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   deletePost(postId: string) {
