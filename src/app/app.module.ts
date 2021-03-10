153838c8636ac3c0944f9f06470eeaa436d57c97
Sone <sthaya@gmail.com>
2021-03-09 17:53:38 -0800

    installed angular material, created header component, styled post create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
+import { HeaderComponent } from './header/header.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
