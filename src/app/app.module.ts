ca3981bca0ae4f0a3096814ca3eeaac372fae96f
Sone <sthaya@gmail.com>
2021-03-15 17:50:22 -0700

    created angular material module to hold their imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AngularMaterialModule } from './angular-material.module';
 import { SuccessInterceptor } from './success-interceptor';
 import { SuccessComponent } from './success/success.component';
 import { ErrorComponent } from './error/error.component';
@@ -15,14 +16,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatDialogModule } from '@angular/material/dialog';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -43,15 +37,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    MatInputModule,
     BrowserAnimationsModule,
-    MatCardModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatToolbarModule,
-    MatProgressSpinnerModule,
-    MatExpansionModule,
-    MatPaginatorModule,
+    AngularMaterialModule,
     HttpClientModule,
   ],
   providers: [
